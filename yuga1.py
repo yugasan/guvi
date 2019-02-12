b0f5d2a01443466f79ef187911d78518a2b323ac
yugasan <47555633+yugasan@users.noreply.github.com>
2019-02-12 15:05:40 +0530

    Update yuga1.py

--- yuga1.py
+++ yuga1.py
@@ -1,9 +1,11 @@
- x=int(input())
-if x<0:
-    print('Negative')
-elif x>0:
-   print('positive')
+x =int(input())
+if x>0:
+  print('Positive')
+elif x<0:
+  print('Negative')
 else:
    print('Zero')
+
+
          
    
